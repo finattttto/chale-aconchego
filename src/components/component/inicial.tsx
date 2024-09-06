4e7588a0bc5982490b8d6903c6fcf017afec87d4
Luan <luanfinatto6@gmail.com>
2024-09-06 20:20:48 -0300

    Ajuste para deploy

--- src/components/component/inicial.tsx
+++ src/components/component/inicial.tsx
@@ -24,8 +24,6 @@ To read more about using these font, please visit the Next.js documentation:
 - Pages Directory: https://nextjs.org/docs/pages/building-your-application/optimizing/fonts
 **/
 import Link from "next/link"
-import { Button } from "@/components/ui/button"
-import { Collapsible, CollapsibleTrigger, CollapsibleContent } from "@/components/ui/collapsible"
 import Footer from "./footer"
 import Perguntas from "./perguntas"
 import Informacoes from "./informacoes"
@@ -159,20 +157,6 @@ function DollarSignIcon(props) {
 
 function MountainIcon(props) {
   return (
-    // <svg
-    //   {...props}
-    //   xmlns="http://www.w3.org/2000/svg"
-    //   width="24"
-    //   height="24"
-    //   viewBox="0 0 24 24"
-    //   fill="none"
-    //   stroke="currentColor"
-    //   strokeWidth="2"
-    //   strokeLinecap="round"
-    //   strokeLinejoin="round"
-    // >
-    //   <path d="m8 3 4 8 5-5 5 15H2L8 3z" />
-    // </svg>
     <img
       {...props}
       src="assets/logo-chale.png"
